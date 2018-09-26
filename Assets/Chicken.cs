a482ad92f243cd3ea981f35a9b1173689adae317
Araz Yusubov <araz_yusubov@acm.org>
2018-09-26 14:44:10 +0400

    Initial
    Initial version

---
+++ Assets/Chicken.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chicken : MonoBehaviour {
+    // Speed of Chicken movement
+    public float speed = 3f;
+
+    // Right or left limit of the movement
+    public float limitMovement = 4f;
+
+    // Chance of random change of direction
+    public float chanceDirectionChange = 0.01f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = transform.position;
+        pos.x = pos.x + speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (Mathf.Abs(pos.x) > limitMovement)
+        {
+            speed = -speed;
+        } else if (Random.value < chanceDirectionChange)
+        {
+            speed = -speed;
+        }
+	}
+}
